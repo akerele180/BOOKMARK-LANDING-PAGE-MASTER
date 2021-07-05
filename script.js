f66a7be8de9f551693ecbcb0ee583a27dca22c45
Tosin Akerele <akerele.james1699@gmail.com>
2021-07-05 01:02:39 +0100

    second round

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-const up = document.querySelectorAll(".up");
-const down = document.querySelectorAll(".down");
-const answers = document.querySelectorAll(".answers");
-const answered = document.querySelectorAll(".answered");
+const up = document.querySelectorAll[1](".up");
+const down = document.querySelector(".down");
+// const answers = document.querySelector(".answers");
+// const answered = document.querySelector(".answered");
 
-
-// function rotate(){
-//   up.classList.toggle('down');
-//   answers.classList.toggle("answered");
-
-//   down.classList.remove("up")
-//   answered.classList.remove("answers");
-
-// }
-
-// function rotated(){
-//   up.classList.remove("down");
-//   answers.classList.remove("answered")
-// }
-// down.addEventListener('click', rotated());
-up.addEventListener('click', (){
+up.addEventListener('click', ()=>{
   up.classList.toggle("down");
+  answers.classList.toggle("answered")
 });
